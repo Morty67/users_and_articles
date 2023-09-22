c5b5bf36d3dea879e2478d41b75369d52c468813
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-09-22 16:53:17 +0300

    Added User model

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+__all__ = ["User"]
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    age = Column(Integer)
+
+    articles = relationship("Article", back_populates="owner")
