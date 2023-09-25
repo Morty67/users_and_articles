d195c79e8c1561ace1355b64d2f2800045422a96
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2023-09-25 16:00:50 +0300

    Updated config.py

--- config.py
+++ config.py
@@ -5,19 +5,32 @@ from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
 
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
-JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
-JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM")
 
 
 SQLALCHEMY_DATABASE_URL = (
     f"postgresql+asyncpg://{DB_USER}:{DB_PASS}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+
+
+DATABASE_URL_TEST = (
+    f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASS_TEST}"
+    f"@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+)
+
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM")
 
 SECRET_KEY = JWT_SECRET_KEY
 ALGORITHM = JWT_ALGORITHM
